df512f26f6e274711578b227d57b0b74b841d464
zchristy <zchristy44@gmail.com>
2019-04-12 12:41:39 -0500

    unfishished carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,49 @@
 class Carousel {
+  constructor(carElement) {
+    this.carElement = carElement;
 
+    this.left = this.carElement.querySelector('.left-button');
+    this.right = this.carElement.querySelector('.right-button');
+
+    this.image = Array.from(this.carElement.querySelectorAll('img'));
+
+    this.counter = 0;
+    this.numImages = this.image.length;
+    this.currentIndex = this.image[0];
+
+    this.carElement.classList.add('active');
+
+    this.navigate = this.navigate.bind(this);
+    this.left.addEventListener('click', function(event) {
+      this.navigate(1);
+    });
+
+    this.right.addEventListener('click', function(event) {
+      this.navigate(-1);
+    });
+  }
+
+  navigate(direction) {
+    this.currentIndex.classList.remove('active');
+    this.counter = this.counter + direction;
+    if (direction === -1 &&
+        this.counter < 0) {
+      this.counter = this.numImages - 1;
+    }
+    if (direction === 1 &&
+        !this.image[this.counter]) {
+      this.counter = 0;
+    }
+    this.currentIndex = this.image[this.counter];
+    this.currentIndex.classList.add('active');
+  }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelectorAll('.carousel');
+
+carousel.forEach(function(carElement) {
+  new Carousel(carElement);
+});
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
